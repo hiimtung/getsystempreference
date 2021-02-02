1fdbc6bfb3547a78a14fa6192eec2744767b7648
hiimtung <kimck1708@gmail.com>
2021-02-02 23:31:13 +0700

    Delete GetSystemSpec_MacOS.sh

--- GetSystemSpec_MacOS.sh
+++
@@ -1,5 +0,0 @@
-system_profiler SPHardwareDataType| sysctl -a |grep machdep.cpu.brand_string | sed 's/machdep.cpu.brand_string/CPU/'>CauHinh.txt
-
-system_profiler SPMemoryDataType| sed '/Empty/d'| sed '/Type/d'| sed '/Speed/d'| sed '/Status/d'| sed '/Part Number/d'>>CauHinh.txt
-system_profiler SPStorageDataType | grep "Device Name" |sed 's/Device Name/Disk/'>>CauHinh.txt
-system_profiler SPDisplaysDataType | grep "Chipset Model" |sed 's/Chipset Model/Card Graphic/'>>CauHinh.txt
