3f1022b665bd53049e926a9386f706806a839e47
hiimtung <kimck1708@gmail.com>
2021-02-03 08:34:01 +0700

    Update GetSystemSpec_Linux.sh

--- GetSystemSpec_Linux.sh
+++ GetSystemSpec_Linux.sh
@@ -1,7 +1,7 @@
 sudo lshw -short > system.txt
-grep 'system' system.txt > cauhinh.txt
-grep 'process' system.txt >> cauhinh.txt
-grep 'memory.*MHz' system.txt >> cauhinh.txt
-grep 'disk' system.txt >> cauhinh.txt
-grep 'display' system.txt >> cauhinh.txt
+grep 'system' system.txt > specs.txt
+grep 'process' system.txt >> specs.txt
+grep 'memory.*MHz' system.txt >> specs.txt
+grep 'disk' system.txt >> specs.txt
+grep 'display' system.txt >> specs.txt
 rm system.txt
